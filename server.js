0f56afc2ff8316dab9fe4430e093f0cf60e20065
Olga-Melnyk <3717129@ukr.net>
2023-05-22 12:33:14 +0300

    add env file

--- server.js
+++ server.js
@@ -2,10 +2,8 @@ const mongoose = require("mongoose");
 
 const app = require("./app");
 
-const DB_HOST =
-  "mongodb+srv://Olga:V9TQZ2qOKzMSF943@cluster0.bgvloy9.mongodb.net/db-contacts?retryWrites=true&w=majority";
 mongoose
-  .connect(DB_HOST)
+  .connect(process.env.DB_HOST)
   .then(() => {
     app.listen(3000, () => {
       console.log("Database connection successful");
