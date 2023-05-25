d871685ac4cdf70e1617425933d416b517193ec5
Olga-Melnyk <3717129@ukr.net>
2023-05-25 16:32:54 +0300

    add authenticate middleware

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -42,7 +42,7 @@ const login = async (req, res) => {
   }
 
   const payload = {
-    id: user._id,
+    contactId: user._id,
   };
 
   const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
